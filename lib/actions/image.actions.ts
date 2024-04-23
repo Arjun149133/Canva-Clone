a6c20296dcb1481732514a32ad8762983ae54678
Arjun <arjun149133@gmail.com>
2024-04-23 23:55:20 +0530

    stripe

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -8,6 +8,7 @@ import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
 
 import { v2 as cloudinary } from "cloudinary";
+
 const populateUser = (query: any) =>
   query.populate({
     path: "author",
@@ -15,15 +16,21 @@ const populateUser = (query: any) =>
     select: "_id firstName lastName clerkId",
   });
 
+// ADD IMAGE
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
 
     const author = await User.findById(userId);
 
-    if (!author) throw new Error("User not found");
+    if (!author) {
+      throw new Error("User not found");
+    }
 
-    const newImage = await Image.create({ ...image, author: author._id });
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    });
 
     revalidatePath(path);
 
@@ -32,16 +39,19 @@ export async function addImage({ image, userId, path }: AddImageParams) {
     handleError(error);
   }
 }
+
+// UPDATE IMAGE
 export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
 
     const imageToUpdate = await Image.findById(image._id);
 
-    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId)
-      throw new Error("Unauthorized or Image not found");
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+      throw new Error("Unauthorized or image not found");
+    }
 
-    const updatedImage = await Image.findOneAndUpdate(
+    const updatedImage = await Image.findByIdAndUpdate(
       imageToUpdate._id,
       image,
       { new: true }
@@ -54,9 +64,12 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
     handleError(error);
   }
 }
+
+// DELETE IMAGE
 export async function deleteImage(imageId: string) {
   try {
     await connectToDatabase();
+
     await Image.findByIdAndDelete(imageId);
   } catch (error) {
     handleError(error);
@@ -64,6 +77,8 @@ export async function deleteImage(imageId: string) {
     redirect("/");
   }
 }
+
+// GET IMAGE
 export async function getImageById(imageId: string) {
   try {
     await connectToDatabase();
@@ -78,14 +93,14 @@ export async function getImageById(imageId: string) {
   }
 }
 
-// GET ALL IMAGES
+// GET IMAGES
 export async function getAllImages({
   limit = 9,
   page = 1,
   searchQuery = "",
 }: {
   limit?: number;
-  page?: number;
+  page: number;
   searchQuery?: string;
 }) {
   try {
@@ -114,15 +129,17 @@ export async function getAllImages({
 
     if (searchQuery) {
       query = {
-        publicId: { $in: resourceIds },
+        publicId: {
+          $in: resourceIds,
+        },
       };
     }
 
-    const skipAmmout = (Number(page) - 1) * limit;
+    const skipAmount = (Number(page) - 1) * limit;
 
     const images = await populateUser(Image.find(query))
       .sort({ updatedAt: -1 })
-      .skip(skipAmmout)
+      .skip(skipAmount)
       .limit(limit);
 
     const totalImages = await Image.find(query).countDocuments();
@@ -132,6 +149,37 @@ export async function getAllImages({
       data: JSON.parse(JSON.stringify(images)),
       totalPage: Math.ceil(totalImages / limit),
       savedImages,
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET IMAGES BY USER
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
     };
   } catch (error) {
     handleError(error);
